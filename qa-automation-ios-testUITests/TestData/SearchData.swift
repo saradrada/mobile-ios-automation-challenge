dd929603c761d99812aa902380cc24e6f60611fa
saradrada <sara.ortizd@perficient.com>
2021-10-11 18:53:04 -0500

    deleted file

--- qa-automation-ios-testUITests/TestData/SearchData.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SearchData.swift
-//  qa-automation-ios-testUITests
-//
-//  Created by Sara Ortiz Drada on 10/10/21.
-//  Copyright © 2021 Lorenzo Bulfone. All rights reserved.
-//
-
-import Foundation
