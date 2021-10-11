1f4366550fa705b1c029ea12d43f8220e4235cb1
saradrada <sara.ortizd@perficient.com>
2021-10-11 00:10:11 -0500

    add LoginTest

---
+++ qa-automation-ios-testUITests/Tests/LoginTest.swift
@@ -0,0 +1,62 @@
+//
+//  LoginTest.swift
+//  qa-automation-ios-testUITests
+//
+//  Created by Sara Ortiz Drada on 10/10/21.
+//  Copyright © 2021 Lorenzo Bulfone. All rights reserved.
+//
+
+import XCTest
+
+class LoginTest: AppStarter, LoginScreen, SearchScreen {
+
+    private let searchExercisesText = "Search exercises"
+    
+    func test_loginWithValidCredentials() {
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+        
+        typeUsername()
+        typePassword()
+        tapOnLogin()
+        
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+    }
+    
+    func test_loginWithInvalidCredentials() {
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+        
+        typeUsername(LoginData.validUsername.rawValue)
+        typePassword(LoginData.invalidPassword.rawValue)
+        tapOnLogin()
+        
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+        
+        tapOnDismiss()
+    }
+    
+    func test_loginWithEmptyCredentials() {
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+        
+        tapOnLogin()
+        
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+        
+        tapOnDismiss()
+        
+        XCTAssertTrue(searchBar.waitForExistence(timeout: 10))
+        XCTAssertEqual(searchBar.label, searchExercisesText)
+        XCTAssertEqual(searchBar.placeholderValue, searchExercisesText)
+    }
+}
